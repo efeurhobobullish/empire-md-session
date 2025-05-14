093911dd4f3f28fada4c902bb08ee8fa5b110b26
𝙴𝚖𝚙𝚒𝚛𝚎 𝚃𝚎𝚌𝚑 <151479661+efeurhobobullish@users.noreply.github.com>
2025-05-14 23:44:29 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '', // Your Mega A/c Email Here
-    password: '', // Your Mega A/c Password Here
+    email: 'empiretech@duck.com', // Your Mega A/c Email Here
+    password: 'totoempire@1B', // Your Mega A/c Password Here
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
