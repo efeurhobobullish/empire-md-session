0b3fa5bedfd55cd5b92f1016f33da19b9d36d081
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2026-03-20 23:22:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import path from 'path';
-
-import qrRoutes from './qr.js';
-import pairRoutes from './pair.js';
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
 const BASE_DIR = process.cwd();
 
+// Import routes
+const qrRoutes = require('./qr');
+const pairRoutes = require('./pair');
+
+// Fix max listeners warning
+require('events').EventEmitter.defaultMaxListeners = 1000;
+
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(BASE_DIR, 'main.html'));
 });
@@ -24,9 +31,13 @@ app.get('/pair', (req, res) => {
     res.sendFile(path.join(BASE_DIR, 'pair.html'));
 });
 
+// Routes
 app.use('/qr', qrRoutes);
 app.use('/code', pairRoutes);
 
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
